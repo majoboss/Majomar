0ad9f351df5da3e946b62151958c87a8547e2583
gaetanoSC <gaetanointile@itispiazza.edu.it>
2022-11-09 12:20:35 +0100

    Modified verify.php

--- Php/Back/Verify.php
+++ Php/Back/Verify.php
@@ -4,16 +4,24 @@
 
     $username = $_POST["username"];
     $password = $_POST["password"];
-
-    $getusers = "SELECT username,password FROM utenti WHERE username=" . $username . " && password=".$password;
+    
+    $getusers = "SELECT username,password FROM utenti WHERE username= '" . $username . "' AND password='".$password."'";
 
     if($sucessful = $connessione->query($getusers))
     {
-        echo "Logged";
+        if($sucessful->num_rows > 0)
+        {
+            header('Location: ../Php/Home.php');
+        }
+        else
+        {
+            header('Location: ../Php/Home.php');
+            echo "Account not exist";
+        }
     }
     else
     {
-        echo "Not logged";
+        echo "Error";
     }
 
 
